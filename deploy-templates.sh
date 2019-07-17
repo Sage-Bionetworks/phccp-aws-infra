3be5ea45b9784287bce6e76218c996902de2cdb6
Thomas Yu <thomasyu888@gmail.com>
2019-07-17 16:16:39 -0700

    Use env variableg

--- deploy-templates.sh
+++ deploy-templates.sh
@@ -8,4 +8,4 @@ S3_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-p
 CODEDEPLOY_APPLICATION=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-phc-codedeploy-CodeDeployS3Bucket'].Value" --output text)
 CODEDEPLOY_DEPLOYMENTGROUP=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-phc-codedeploy-DeploymentGroup'].Value" --output text)
 
-aws deploy create-deployment --application-name CODEDEPLOY_APPLICATION --deployment-config-name CodeDeployDefault.AllAtOnce --deployment-group-name CODEDEPLOY_DEPLOYMENTGROUP --s3-location bucket=S3_BUCKET,key=deploy.zip,bundleType=zip
+aws deploy create-deployment --application-name $CODEDEPLOY_APPLICATION --deployment-config-name CodeDeployDefault.AllAtOnce --deployment-group-name $CODEDEPLOY_DEPLOYMENTGROUP --s3-location bucket=$S3_BUCKET,key=deploy.zip,bundleType=zip
