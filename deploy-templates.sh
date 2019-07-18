40e370754f4d61bfd8843065af847d8dbbd82e45
thomasyu888 <thomasyu888@gmail.com>
2019-07-18 10:44:35 -0700

    Add deploy.zip into s3

--- deploy-templates.sh
+++ deploy-templates.sh
@@ -8,4 +8,5 @@ S3_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-p
 CODEDEPLOY_APPLICATION=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-phc-codedeploy-CodeDeployApplication'].Value" --output text)
 CODEDEPLOY_DEPLOYMENTGROUP=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-phc-codedeploy-DeploymentGroup'].Value" --output text)
 
+aws s3 cp target/deploy.zip s3://$S3_BUCKET/deploy.zip
 aws deploy create-deployment --application-name $CODEDEPLOY_APPLICATION --deployment-config-name CodeDeployDefault.AllAtOnce --deployment-group-name $CODEDEPLOY_DEPLOYMENTGROUP --s3-location bucket=$S3_BUCKET,key=deploy.zip,bundleType=zip
