3037fb442c9e0413e309ecc357e61d42c481cb2a
thomasyu888 <thomasyu888@gmail.com>
2019-07-18 08:58:01 -0700

    Fix code deploy application

--- deploy-templates.sh
+++ deploy-templates.sh
@@ -5,7 +5,7 @@ sed -i "s/DOCKERREGISTRY/$DOCKER_REGISTRY/g" deploy.sh
 zip target/deploy.zip deploy.sh docker-compose.yml appspec.yml
 
 S3_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-phc-codedeploy-essentials-CodeDeployS3Bucket'].Value" --output text)
-CODEDEPLOY_APPLICATION=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-phc-codedeploy-CodeDeployS3Bucket'].Value" --output text)
+CODEDEPLOY_APPLICATION=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-phc-codedeploy-CodeDeployApplication'].Value" --output text)
 CODEDEPLOY_DEPLOYMENTGROUP=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-phc-codedeploy-DeploymentGroup'].Value" --output text)
 
 aws deploy create-deployment --application-name $CODEDEPLOY_APPLICATION --deployment-config-name CodeDeployDefault.AllAtOnce --deployment-group-name $CODEDEPLOY_DEPLOYMENTGROUP --s3-location bucket=$S3_BUCKET,key=deploy.zip,bundleType=zip
