341341162c9616cb8ebac31cabc105f0f9be6512
thomasyu888 <thomasyu888@gmail.com>
2019-07-18 13:06:53 -0700

    Log into docker and comment out

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /tmp
-echo DOCKERUSER DOCKERPASSWORD DOCKERREGISTRY
 echo Stopping docker-compose
 docker-compose down || true
 echo Starting docker-compose
+docker login DOCKERREGISTRY -u DOCKERUSER -p DOCKERPASSWORD
 docker-compose pull && docker-compose up -d
