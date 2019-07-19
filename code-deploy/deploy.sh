784ff43907381c150107e7bd5fb81743b48ddc65
Thomas Yu <thomasyu888@gmail.com>
2019-07-19 14:23:30 -0700

    Must remove double quotes

--- code-deploy/deploy.sh
+++ code-deploy/deploy.sh
@@ -4,9 +4,9 @@ echo Stopping docker-compose
 docker-compose down || true
 echo Starting docker-compose
 
-DOCKERREGISTRY=$(aws ssm get-parameters --region us-east-1 --names DOCKER_REGISTRY --with-decryption --query Parameters[0].Value)
-DOCKERUSER=$(aws ssm get-parameters --region us-east-1 --names DOCKER_USER --with-decryption --query Parameters[0].Value)
-DOCKERPASSWORD=$(aws ssm get-parameters --region us-east-1 --names DOCKER_PASSWORD --with-decryption --query Parameters[0].Value)
+DOCKERREGISTRY=$(aws ssm get-parameters --region us-east-1 --names DOCKER_REGISTRY --with-decryption --query Parameters[0].Value | sed 's\"\\g')
+DOCKERUSER=$(aws ssm get-parameters --region us-east-1 --names DOCKER_USER --with-decryption --query Parameters[0].Value | sed 's\"\\g')
+DOCKERPASSWORD=$(aws ssm get-parameters --region us-east-1 --names DOCKER_PASSWORD --with-decryption --query Parameters[0].Value | sed 's\"\\g')
 
 export CERTS_DIR=certs
 mkdir $CERTS_DIR
