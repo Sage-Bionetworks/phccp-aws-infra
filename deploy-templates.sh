a9b8f7f01b6f2c6244fdddf892d5d9377deef43d
Thomas Yu <thomasyu888@gmail.com>
2019-07-19 15:57:50 -0700

    Revert back to 443

--- deploy-templates.sh
+++ deploy-templates.sh
@@ -7,8 +7,6 @@ CODEDEPLOY_DEPLOYMENTGROUP=$(aws cloudformation list-exports --query "Exports[?N
 
 #aws s3 cp target/deploy.zip s3://$S3_BUCKET/deploy.zip
 
-
-
 aws deploy push --application-name $CODEDEPLOY_APPLICATION --description "This is a revision for the PHC App" --ignore-hidden-files --s3-location s3://$S3_BUCKET/deploy.zip --source code-deploy
 
 aws deploy create-deployment --application-name $CODEDEPLOY_APPLICATION --deployment-config-name CodeDeployDefault.AllAtOnce --deployment-group-name $CODEDEPLOY_DEPLOYMENTGROUP --s3-location bucket=$S3_BUCKET,key=deploy.zip,bundleType=zip
